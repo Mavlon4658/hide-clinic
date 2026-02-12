e1616f027520fd1a0223e3487dd50ff81a8bf5ea
Mavlon4658 <mavlonbe4658>
2026-02-12 12:27:02 +0500

    contact is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ const phoneInp = document.querySelectorAll('input[type="tel"]');
 if (phoneInp.length) {
     phoneInp.forEach(el => {
         IMask(el, {
-            mask: '+{7}(000) 000-00-00',
+            mask: '+{9}00 00 000 0000',
         })
     });
 }
